f3dc9b81c31238ffd00dc39ecd8cf53d1bad2e6c
Sakshi Kushwaha <100464878+sakshi-kushwaha@users.noreply.github.com>
2023-08-03 10:06:11 +0530

    Create A_7-1_BST.cpp

---
+++ A_7-1_BST.cpp
@@ -0,0 +1,64 @@
+//Demonstrate the following concepts by using C++
+//Binary Search Tree
+#include <iostream>
+using namespace std;
+
+// BST Node definition
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// Function to insert a new node into the BST
+Node* insert(Node* root, int value) {
+    if (root == nullptr) {
+        return new Node(value);
+    }
+
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+// Function to perform in-order traversal of the BST (to display the elements in ascending order)
+void inorderTraversal(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    inorderTraversal(root->left);
+    cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+
+    // Inserting elements into the BST
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    // Displaying elements in ascending order using in-order traversal
+    cout << "BST in ascending order: ";
+    inorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
